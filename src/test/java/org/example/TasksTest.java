25c64b4b6526d832034d9e8e84aceee589da742e
kris-i-kris <flare120491@gmail.com>
2023-09-14 08:10:52 +0700

    Initial commit

---
+++ src/test/java/org/example/TasksTest.java
@@ -0,0 +1,76 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TasksTest {
+    @Test
+    public void containsQuerySimpleTaskTrue() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        boolean expected = true;
+        boolean actual = simpleTask.matches("родителям");
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void containsQueryEpicTrue() {
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        boolean expected = true;
+        boolean actual = epic.matches("Молоко");
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void containsQueryMeetingTrue() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+       // meeting.matches("приложение");
+        boolean expected = true;
+        boolean actual = meeting.matches("Приложение");
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void containsQuerySimpleTaskFalse() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        boolean expected = false;
+        boolean actual = simpleTask.matches("привет");
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void containsQueryEpicFalse() {
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        boolean expected = false;
+        boolean actual = epic.matches("привет");
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void containsQueryMeetingFalse() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        // meeting.matches("приложение");
+        boolean expected = false;
+        boolean actual = meeting.matches("привет");
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
